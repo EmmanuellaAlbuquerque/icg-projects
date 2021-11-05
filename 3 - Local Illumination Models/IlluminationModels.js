7ffce254a9ea82fbec5b1c2d65b886e6835d42a4
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2021-11-04 21:21:53 -0300

    fixing V:(normalized vector pointing to the camera) value

--- 3 - Local Illumination Models/IlluminationModels.js
+++ 3 - Local Illumination Models/IlluminationModels.js
@@ -102,6 +102,12 @@ PhongModels.Gouraud.vertexShader = `
         
         vec4 P_cam_spc = modelViewMatrix * vec4(position, 1.0);
 
+        // 'viewMatrix' : variável de sistema que contém a matriz View (4x4).
+        // 'cameraPosition' : Posição da Câmera no Espaço do Universo.
+        
+        vec4 position_cam_spc = viewMatrix * vec4(cameraPosition, 1.0);
+        vec3 V = normalize(position_cam_spc.xyz - P_cam_spc.xyz);
+
         // 'normal' : variável de sistema que contém o vetor normal do vértice (vec3) no espaço do objeto.
         // 'normalMatrix' : variável de sistema que contém a matriz de normais (3x3) gerada a partir da matriz 'modelViewMatrix'.
         
@@ -115,7 +121,7 @@ PhongModels.Gouraud.vertexShader = `
         // 'reflect()' : função do sistema que retorna 'R_cam_spc', isto é, o vetor 'L_cam_spc' refletido 
         //     em relação o vetor 'N_cam_spc'.
         
-        vec3 R_cam_spc = reflect(L_cam_spc, N_cam_spc);
+        vec3 R_cam_spc = reflect(-L_cam_spc, N_cam_spc);
 
         ///////////////////////////////////////////////////////////////////////////////
         //
@@ -130,7 +136,7 @@ PhongModels.Gouraud.vertexShader = `
         
         vec3 ambient_term = Ia * k_a;
         vec3 diffuse_term = Ip_diffuse_color * k_d * max(0.0, dot(N_cam_spc, L_cam_spc));
-        vec3 specular_term = Ip_diffuse_color * k_s * pow(dot(R_cam_spc, normal), n);
+        vec3 specular_term = Ip_diffuse_color * k_s * pow(max(0.0, dot(R_cam_spc, V)), n);
 
         I = vec4(ambient_term + diffuse_term + specular_term, 1.0);
 
@@ -175,18 +181,10 @@ PhongModels.Phong.vertexShader = `
         normalM3 = normalMatrix;
         normalVec3 = normal;
         positionVec3 = position;
-    
-        // 'position' : variável de sistema que contém a posição do vértice (vec3) no espaço do objeto.
-        // 'P_cam_spc' : variável que contém o vértice (i.e. 'position') transformado para o Espaço de Câmera.
-        //     Observe que 'position' é um vetor 3D que precisou ser levado para o espaço projetivo 4D 
-        //     (i.e., acrescentando-se uma coordenada adicional w = 1.0) para poder ser multiplicado pela
-        //     matriz 'modelViewMatrix' (que é 4x4).
-        
-        vec4 P_cam_spc = modelViewMatrix * vec4(position, 1.0);
 
         // 'gl_Position' : variável de sistema que conterá os vértice no espaço de recorte.
         
-        gl_Position = projectionMatrix * P_cam_spc;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
     }
     `;
 
@@ -232,13 +230,19 @@ PhongModels.Phong.fragmentShader = `
         
         vec4 Ip_pos_cam_spc = modelViewM4 * vec4(Ip_position, 1.0);
 
-        // 'position' : variável de sistema que contém a posição do vértice (vec3) no espaço do objeto.
+        // 'positionVec3' : variável de sistema que contém a posição do vértice (vec3) no espaço do objeto.
         // 'P_cam_spc' : variável que contém o vértice (i.e. 'position') transformado para o Espaço de Câmera.
         //     Observe que 'position' é um vetor 3D que precisou ser levado para o espaço projetivo 4D 
         //     (i.e., acrescentando-se uma coordenada adicional w = 1.0) para poder ser multiplicado pela
         //     matriz 'modelViewMatrix' (que é 4x4).
         
         vec4 P_cam_spc = modelViewM4 * vec4(positionVec3, 1.0);
+
+        // 'viewMatrix' : variável de sistema que contém a matriz View (4x4).
+        // 'cameraPosition' : Posição da Câmera no Espaço do Universo.
+
+        vec4 position_cam_spc = viewMatrix * vec4(cameraPosition, 1.0);
+        vec3 V = normalize(position_cam_spc.xyz - P_cam_spc.xyz);
 
         // 'normalVec3' : variável de sistema que contém o vetor normal do vértice (vec3) no espaço do objeto.
         // 'normalM3' : variável de sistema que contém a matriz de normais (3x3) gerada a partir da matriz 'modelViewMatrix'.
@@ -253,7 +257,7 @@ PhongModels.Phong.fragmentShader = `
         // 'reflect()' : função do sistema que retorna 'R_cam_spc', isto é, o vetor 'L_cam_spc' refletido 
         //     em relação o vetor 'N_cam_spc'.
         
-        vec3 R_cam_spc = reflect(L_cam_spc, N_cam_spc);
+        vec3 R_cam_spc = reflect(-L_cam_spc, N_cam_spc);
 
         // 'I' : cor final (i.e. intensidade) do vértice.
         //     Neste caso, a cor retornada é vermelho. Para a realização do exercício, o aluno deverá atribuir a 'I' o valor
@@ -261,10 +265,10 @@ PhongModels.Phong.fragmentShader = `
         
         vec3 ambient_term = Ia * k_a;
         vec3 diffuse_term = Ip_diffuse_color * k_d * max(0.0, dot(N_cam_spc, L_cam_spc));
-        vec3 specular_term = Ip_diffuse_color * k_s * pow(dot(R_cam_spc, normalVec3), n);
+        vec3 specular_term = Ip_diffuse_color * k_s * pow(max(0.0, dot(R_cam_spc, V)), n);
 
         I = vec4(ambient_term + diffuse_term + specular_term, 1.0);
-        // I = vec4(k_a, 1.0);
+        // I = viewMatrix * vec4(cameraPosition, 1.0);
     
         // 'gl_FragColor' : variável de sistema que conterá a cor final do fragmento calculada pelo Fragment Shader.
         
@@ -288,6 +292,8 @@ let material = new THREE.ShaderMaterial({
   vertexShader: PhongModels.Phong.vertexShader,
   fragmentShader: PhongModels.Phong.fragmentShader
 });
+
+// renderer.setClearColor(0xffffff);
 
 //----------------------------------------------------------------------------
 // 'object_mesh' : De acordo com o Three.js, um 'mesh' é a geometria acrescida do material.
