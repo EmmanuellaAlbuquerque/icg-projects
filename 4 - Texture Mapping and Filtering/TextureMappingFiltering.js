8db76563df1086f4345bfd1719eef3f2fedfe20e
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2021-11-15 18:55:22 -0300

    adding some textures

--- 4 - Texture Mapping and Filtering/TextureMappingFiltering.js
+++ 4 - Texture Mapping and Filtering/TextureMappingFiltering.js
@@ -4,15 +4,12 @@ let images = {
   checkerboard: src_checkerboard,
   arrow: 'images/magnification/Back-Arrow.png',
   sword: 'images/magnification/Sword.png',
-  sphere: 'images/magnification/Wood_Sphere.png',
   deck: 'images/magnification/Wooden_Decking.png',
   cartoon_roof: 'images/minification/Cartoon_Roof.png',
   floor: 'images/minification/Forest_Floor.jpeg',
-  panels_wall: 'images/minification/Panels_Tiles_Wall.jpeg',
-  panels_tiles: 'images/minification/Panels_Tiles.png',
-  roof_tiles: 'images/minification/Roof_Tiles.jpg',
   roof: 'images/minification/Roof.jpg',
-  wood_sphere: 'images/minification/WoodSphere.png',
+  brown_rock: 'images/minification/Brown_Rock.jpg',
+  brick: 'images/minification/Brick.jpg'
 };
 
 let image = new Image();
@@ -20,28 +17,28 @@ image.src = images.gate; // textura do portão do Doom (mag)
 image.src = images.checkerboard; // textura do padrão xadrez (mag)
 image.src = images.arrow; // textura de uma seta para trás (mag)
 image.src = images.sword; // textura de uma espada (mag)
-image.src = images.sphere; // textura de uma esfera (mag)
 image.src = images.deck; // textura de um deck de madeira (mag)
 image.src = images.cartoon_roof; // textura de um telhado (min)
 image.src = images.floor; // textura do chão de um parque (min)
-image.src = images.panels_wall; // textura de azulejos de parede (min)
-image.src = images.panels_tiles; // textura do azulejos acolchoado (min)
-image.src = images.roof_tiles; // textura de um telhado branco (min)
 image.src = images.roof; // textura de um telhado marrom (min)
-image.src = images.wood_sphere; // textura de uma esfera (min)
+image.src = images.brown_rock; // textura de um pedra marrom (min)
+image.src = images.brick; // textura de um pedra marrom (min)
 
 texture = new THREE.Texture(image);
 
 // Filtros para Magnificação e Minificação
 let filters = {
+  NearestNeighbor: THREE.NearestFilter,
   Bilinear: THREE.LinearFilter,
   Mipmapping: THREE.LinearMipmapLinearFilter,
-  NearestNeighbor: THREE.NearestFilter,
+  MipmappingNearest: THREE.NearestMipmapNearestFilter,
+  MipmappingNearestLinear: THREE.NearestMipmapLinearFilter,
+  MipmappingNearestBilinear: THREE.LinearMipmapNearestFilter
 };
 
 image.onload = function () {
   texture.needsUpdate = true;
-  texture.magFilter = filters.Bilinear; // filtro a ser utilizado em caso de magnificação.
+  texture.magFilter = filters.NearestNeighbor; // filtro a ser utilizado em caso de magnificação.
   texture.minFilter = filters.NearestNeighbor; // filtro a ser utilizado em caso de minificação.
   texture.anisotropy = 1; // fator máximo de anisotropia para o filtro anisotrópico.
   texture.wrapS = THREE.RepeatWrapping;
@@ -51,8 +48,19 @@ image.onload = function () {
 let scene = new THREE.Scene();
 
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 100);
-camera.position.z = 1;
-camera.position.y = -8;
+
+// position 1 for BoxGeometry (CUBE)
+// camera.position.z = 1;
+// camera.position.x = 1;
+// camera.position.y = 1;
+
+// position 2 for PlaneGeometry (PLANE)
+// camera.position.z = 0.5;
+// camera.position.y = -7.5;
+
+// position 3 for PlaneGeometry (PLANE)
+camera.position.z = 0.3;
+camera.position.y = -7;
 
 scene.add(camera);
 
